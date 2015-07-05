7d05f5abb2b1f19ac00c30d6947f7fbc34d8f28a
zx648383079 <648383079@qq.com>
2015-07-05 14:25:30 +0800

    上传源码

---
+++ 正则提取/ChooseWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+
+namespace 正则提取
+{
+    /// <summary>
+    /// Interaction logic for ChooseWindow.xaml
+    /// </summary>
+    public partial class ChooseWindow : Window
+    {
+        public ChooseWindow()
+        {
+            InitializeComponent();
+        }
+
+        public new string Content { get; set; }
+
+        private void Nobtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Yesbtn_Click(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetData(DataFormats.Text, ContentTb.Text);//复制内容到剪切板
+            Savetb.Text = "已成功复制到剪切板！";
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ContentTb.Text = Content;
+        }
+    }
+}
