a0214790d6060bafcc9310efa7e8d8eacf5580cd
zx648383079 <648383079@qq.com>
2016-03-23 18:53:33 +0800

    修改语法结构

--- 正则提取/MainWindow.xaml.cs
+++ 正则提取/MainWindow.xaml.cs
@@ -1,9 +1,14 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Globalization;
 using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Documents;
 using System.Windows.Input;
+using 正则提取.Model;
 
 namespace 正则提取
 {
@@ -12,78 +17,129 @@ namespace 正则提取
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly ObservableCollection<MatchItem> _lists = new ObservableCollection<MatchItem>();
+        private MatchCollection _matches;
+
         public MainWindow()
         {
             InitializeComponent();
+            Listbox.ItemsSource = _lists;
         }
 
         private void StartBtn_Click(object sender, RoutedEventArgs e)
         {
-            Listbox.Items.Clear();
-            if (MatchTb.Text.Trim() == "") return;
-            MatchCollection ms = Regex.Matches(MatchTb.Text, RegexTb.Text);
-            foreach (Match m in ms)
+            _lists.Clear();
+            if (string.IsNullOrWhiteSpace(MatchTb.Text)) return;
+            _matches = Regex.Matches(MatchTb.Text, RegexTb.Text);
+            foreach (Match m in _matches)
             {
+                int index = 0;
                 foreach (Group item in m.Groups)
                 {
-                    Listbox.Items.Add(item.Value);
+                    _lists.Add(new MatchItem(index, item.Value));
+                    index ++;
                 }
             }
-            CountLb.Text = "添加了 " + Listbox.Items.Count.ToString(CultureInfo.InvariantCulture) + " 条数据";
+            CountLb.Text = "添加了 " + _lists.Count.ToString(CultureInfo.InvariantCulture) + " 条数据";
         }
 
         private void Listbox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
-            int sum = Listbox.Items.Count;
-            int index = Listbox.SelectedIndex;
-            CountLb.Text = "当前状态："+(index+1).ToString(CultureInfo.InvariantCulture)+"/"+sum.ToString(CultureInfo.InvariantCulture);
-            if (index < 0 || index >= sum) return;
-            string temp = ReplaceTb.Text;
-            if (temp.IndexOf("$foreach", System.StringComparison.Ordinal)>=0)
+            if (_lists.Count < 1) return;
+            var pattern = @"{for(\(([\d,]+)\))?([\s\S]+?)end}|{([\w\.]+?)}";
+            var content = ReplaceTb.Text;
+            Task.Factory.StartNew(() =>
             {
-                Match m= Regex.Match(temp, @"^(\$foreach)(?<Text>.*?)(end\$)$");
-                string temp1 = m.Groups["Text"].Value;
-                string text = Listbox.Items.Cast<object>().Aggregate("", (current, t) => current + (temp1.Replace("$$", t.ToString()) + "\n"));
-                temp = temp.Replace(m.Value, text);
-            }
-            else if (temp.IndexOf("$for", System.StringComparison.Ordinal) >=0)
-            {
-                Match m = Regex.Match(temp, @"^(\$for)(?<Text>[\s\S]*?)(end\$)$");      //正则表达式匹配任意字符（包括换行符）的写法:([\s\S]*) 同时，也可以用 “([\d\D]*)”、“([\w\W]*)” 来表示。
-                string temp1 = m.Groups["Text"].Value;
-                int count= Regex.Matches(temp1, @"\$\$").Count;
-                if (count==sum)
+                var matches = Regex.Matches(content, pattern);
+                foreach (Match item in matches)
                 {
-                    Regex reg = new Regex(@"\$\$");
-                    for (int i = 0; i < count; i++)
+                    var replace = string.Empty;
+                    if (string.IsNullOrWhiteSpace(item.Groups[3].Value))
                     {
-                        temp1=reg.Replace(temp1,Listbox.Items[i].ToString(),1);
+                        string[] keys = item.Groups[4].Value.Split('.');
+                        if (keys.Length == 1)
+                        {
+                            replace = _matches[int.Parse(keys[0])].Value;
+                        }
+                        else
+                        {
+                            var key = keys[1];
+                            replace = IsNumberic(key)
+                                ? _matches[int.Parse(keys[0])].Groups[int.Parse(key)].Value
+                                : _matches[int.Parse(keys[0])].Groups[key].Value;
+                        }
                     }
-                    temp = temp.Replace(m.Value, temp1);
+                    else
+                    {
+                        int start = 0;
+                        int length = _matches.Count;
+                        if (!string.IsNullOrWhiteSpace(item.Groups[2].Value))
+                        {
+                            string[] nums = item.Groups[2].Value.Split(',');
+                            if (nums.Length == 1)
+                            {
+                                length = Math.Min(length, int.Parse(nums[0]));
+                            }
+                            else
+                            {
+                                start = int.Parse(nums[0]);
+                                length = Math.Min(length - start, int.Parse(nums[1]));
+                            }
+                        }
+                        var text = item.Groups[3].Value;
+                        var replaces = new StringBuilder();
+                        var textMatches = Regex.Matches(text, @"{(\w*?)}");
+                        for (int i = 0; i < length; i++)
+                        {
+                            var replacedText = text;
+                            Match itemm = _matches[start + i];
+                            foreach (Match textMatch in textMatches)
+                            {
+                                var key = textMatch.Groups[1].Value;
+                                if (IsNumberic(key))
+                                {
+                                    int index = 0;
+                                    if (!string.IsNullOrWhiteSpace(key))
+                                    {
+                                        index = int.Parse(key);
+                                    }
+                                    replacedText = replacedText.Replace(textMatch.Value, itemm.Groups[index].Value);
+                                }
+                                else
+                                {
+                                    replacedText = replacedText.Replace(textMatch.Value, itemm.Groups[key].Value);
+                                }
+                                //replacedText = replacedText.Replace(textMatch.Value, IsNumberic(key) ? item.Groups[int.Parse(key)].Value : item.Groups[key].Value);
+                            }
+                            replaces.Append(replacedText);
+                        }
+                        replace = replaces.ToString();
+                    }
+                    content = content.Replace(item.Value, replace);
                 }
-                else
+                Dispatcher.Invoke(() =>
                 {
-                    MessageBox.Show("需要替换的地方有"+count.ToString(CultureInfo.InvariantCulture)+"个\n列表中有"+sum.ToString(CultureInfo.InvariantCulture)+"个", "提示");
-                }
-            }
-            else
-            {
-                temp = temp.Replace("$$", Listbox.Items[index].ToString());
-            }
-            ChooseWindow chooseWindow = new ChooseWindow {Content = temp};
-            chooseWindow.Show();
+                    var chooseWindow = new ChooseWindow {Content = content};
+                    chooseWindow.Show();
+                });
+            });
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            //AbouLb.Text = "关于\n用$$代替要替换的内容；可以使用\n$foreach $$ end$\n输出列表中所有内容。";
             AbouLb.Inlines.Add(new Bold(new Run("   关于")));
-            AbouLb.Inlines.Add(new Run("\n用"));
-            AbouLb.Inlines.Add(new Italic(new Run("$$")));
-            AbouLb.Inlines.Add(new Run("代替要替换的内容；可以使用"));
-            AbouLb.Inlines.Add(new Italic(new Run("\n$foreach $$ end$\n")));
-            AbouLb.Inlines.Add(new Run("输出列表中所有内容，还可以用"));
-            AbouLb.Inlines.Add(new Italic(new Run("\n$for $$ end$\n")));
-            AbouLb.Inlines.Add(new Run("输出列表中所有内容，但必须保证要替换的个数与列表个数相同。"));
+            AbouLb.Inlines.Add(new Run("\n包含两种用法："));
+            AbouLb.Inlines.Add(new Italic(new Run("")));
+            AbouLb.Inlines.Add(new Run("1.普通输出{} 可以应{1.tt}输出第二个匹配中tt标志的内容"));
+            AbouLb.Inlines.Add(new Italic(new Run("\n2.循环输出{for end}\n")));
+            AbouLb.Inlines.Add(new Run("for 无参数时输出所有"));
+            AbouLb.Inlines.Add(new Italic(new Run("\nfor(10) 一个参数时，从第一个开始输出10个\n")));
+            AbouLb.Inlines.Add(new Run("for(1,10) 两个参数时，从第二个开始输出10个"));
+        }
+
+        protected bool IsNumberic(string message)
+        {
+            return Regex.IsMatch(message, @"^\d*$");
         }
     }
 }
---
+++ 正则提取/Model/MatchItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 正则提取.Model
+{
+    public class MatchItem
+    {
+        public int Tag { get; set; }
+
+        public string Content { get; set; }
+
+        public MatchItem()
+        {
+            
+        }
+
+        public MatchItem(int tag, string content)
+        {
+            Tag = tag;
+            Content = content;
+        }
+    }
+}
